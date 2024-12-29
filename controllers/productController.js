01553979808c4db37013a31e96a31bc691eeb3d0
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-12-29 12:06:13 +0530

    create product controller

---
+++ controllers/productController.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createProduct = async (req, res) => {
+  try {
+    if (!req.body.name) {
+      return res.status(422).json({ error: "Name is required" });
+    }
+    if (!req.body.price) {
+      return res.status(422).json({ error: "Price is required" });
+    } else {
+      if (typeof req.body.price !== "number" || req.body.price < 0) {
+        return res
+          .status(422)
+          .json({ error: "Price must be a non negative number" });
+      }
+    }
+    if (!req.body.categoryId) {
+      return res.status(422).json({ error: "Category id is required" });
+    } else {
+      if (
+        !(await prisma.category.findUnique({
+          where: { id: parseInt(req.body.categoryId) },
+        }))
+      ) {
+        return res.status(404).json({ error: "Category id not found" });
+      }
+    }
+
+    const newProduct = await prisma.product.create({
+      data: req.body,
+    });
+
+    return res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in create product controller:", error.message);
+  }
+};
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import categoryRoutes from "./routes/categoryRoute.js";
+import productRoutes from "./routes/productRoute.js";
 
 const app = express();
 app.use(express.json());
+
+app.use("/products", productRoutes);
 app.use("/categories", categoryRoutes);
 
 app.listen(3000, () => console.log("Server started on port 3000"));
